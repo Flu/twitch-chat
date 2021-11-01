32e117d912795bc147ff9197163045741afec09a
Adrian Fluturel <adrian.fluturel@protonmail.com>
2021-11-02 00:46:40 +0200

    Add colors based on name

--- Worker.cs
+++ Worker.cs
@@ -1,6 +1,8 @@
 using System;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 using TwitchBot.Models;
 using TwitchBot.Services;
@@ -19,13 +21,26 @@ namespace TwitchBot
         public Task StartAsync(CancellationToken cancellationToken)
         {
             _twitchService.OnMessage += OnMessageCallback;
-            _twitchService.StartListening("squishymuffinz");
+            _twitchService.StartListening("wardell");
             return Task.CompletedTask;
+        }
+
+        public string ColoredBoldName(string name)
+        {
+            using (var sha = new SHA256Managed())
+            {
+                byte[] textBytes = Encoding.UTF8.GetBytes(name);
+                byte[] hashBytes = sha.ComputeHash(textBytes);
+
+                var i = BitConverter.ToUInt64(hashBytes, 0) % 7 + 1;
+
+                return $"\x1b[3{i}m\x1b[1m{name}\x1b[0m\x1b[39m";
+            }
         }
 
         public void OnMessageCallback(object sender, TwitchMessageEventArgs args)
         {
-            Console.WriteLine($"\x1b[1m{args.User}\x1b[0m: {args.Message}");
+            Console.WriteLine($"{ColoredBoldName(args.User)}: {args.Message}");
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
